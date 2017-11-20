faac6788a3c39994bf9900c6bf202957c4f2af75
Peter Ramsing <peter.ramsing@mac.com>
2017-11-19 23:24:09 -0800

    Adds an auto-scroll for the messages

--- frontend/src/app/conversation-panel/conversation-panel.component.ts
+++ frontend/src/app/conversation-panel/conversation-panel.component.ts
@@ -22,23 +22,41 @@ export class ConversationPanelComponent implements OnInit, AfterViewInit {
     this.eBayListings = [];
     this.messageLog = [];
     this.messageValue = '';
+    this.resultsPresent = false;
     this.submitMessage(); // Initial call on load to start the conversation
   }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.convoListElement = document.querySelector("#js-ConversationList")
+  }
+
+  _scrollTop() {
+    if (this.convoListElement) {
+      // Using a timeout to ensure the render is complete... 😳
+      setTimeout(() => {
+        this.convoListElement.scrollTop = this.convoListElement.scrollHeight;
+      }, 500);
+    }
+  }
+
   _searchEBay(context) {
     console.log('searching ebay', context)
     this._ES.searchEBayRequest(context)
       .subscribe((data:any) => {
-        this.eBayListings = data
+        this.eBayListings = data;
+        this.resultsPresent = true;
         // TODO: Submit another Watson Request to trigger emailing these
         // when implementing the email/text service
       });
   }
 
   submitMessage() {
+    this._scrollTop();
+
+
     if (this.messageValue) {
       this.messageLog.push({
         'text': this.messageValue,
