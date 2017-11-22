814191722b3e963e53358ad64ce41f5fdc6470eb
Peter Ramsing <peter.ramsing@mac.com>
2017-11-21 22:30:32 -0800

    Adds ability to submit initial page view

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { AnalyticsService } from './core/analytics.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  constructor(
+    private _analytics: AnalyticsService,
+  ) {
+    this._analytics.pageview('/');
+  }
 }
